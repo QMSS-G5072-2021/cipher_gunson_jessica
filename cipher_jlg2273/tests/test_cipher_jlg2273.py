188cfd5e6ddbd145e3fc55504c370c4f9d232104
jessicagunson <jessicagunson12@gmail.com>
2021-11-10 11:29:34 -0500

    Add tests

--- cipher_jlg2273/tests/test_cipher_jlg2273.py
+++ cipher_jlg2273/tests/test_cipher_jlg2273.py
@@ -1,4 +1,20 @@
-from cipher_jlg2273 import cipher_jlg2273
+
+
+import pytest
+
+def cipher(text, shift, encrypt=True):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+    assert isinstance(shift, int), "Shift should be numerical"
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
 
 def test_cipher_with_nonalphabet():
     text = 'apple!'
