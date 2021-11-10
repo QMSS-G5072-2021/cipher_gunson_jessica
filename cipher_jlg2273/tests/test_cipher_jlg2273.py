57771081290c3c806688b9c69591ff4b8092d95e
jessicagunson <jessicagunson12@gmail.com>
2021-11-10 11:12:51 -0500

    Update test_cipher_jlg2273.py

--- cipher_jlg2273/tests/test_cipher_jlg2273.py
+++ cipher_jlg2273/tests/test_cipher_jlg2273.py
@@ -1,1 +1,8 @@
 from cipher_jlg2273 import cipher_jlg2273
+
+def test_cipher_with_nonalphabet():
+    text = 'apple!'
+    shift = 1
+    expected = 'bqqmf!'
+    actual = cipher(text, shift)
+    assert actual == expected
